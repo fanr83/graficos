fc44d7159cefc939e4f2ac39f6e5901b6a182e15
fanr83 <75618793+fanr83@users.noreply.github.com>
2020-12-28 21:47:26 -0300

    Initial commit

---
+++ color.R
@@ -0,0 +1,5 @@
+# mira esa wea hermano
+mswh = c( #5A9DC8, #007CB8, #015593, #013281, #722142, #C8A115, #B27362, #66342D, #21201C )
+
+# me pase poh (2016)
+mpp = c ( #FCFCFE, #CAC3CB, #4C4B5B, #0F0810, #D49286, #EDADA1, #E6D9C6, #F97C5C, #CD2B36 )
